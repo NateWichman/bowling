9eb41f2555c13e50f919ac93d554bf1a04b46ca6
Nathan Cale Wichman <wichmann@mail.gvsu.edu>
2022-01-31 10:16:14 -0500

    code and stuff

---
+++ Assets/Pin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    public GameObject PinHeight;
+    // Start is called before the first frame update
+
+    public bool hasFallen = false;
+    void Start()
+    {
+    }
+
+    public void Destroy()
+    {
+        GameObject.Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!hasFallen && (PinHeight.transform.position.y < GameManager.Instance.PinHeight
+            || PinHeight.transform.position.y > GameManager.Instance.PinMax))
+        {
+            GameManager.Instance.PinFall();
+            hasFallen = true;
+        }
+    }
+}
