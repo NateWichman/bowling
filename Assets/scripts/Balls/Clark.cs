23c8d7c4b840d380af7381999c161eede1649acd
Nate Wichman <nwichman@ostusa.com>
2022-08-29 19:27:33 -0400

    ball

--- Assets/scripts/Balls/Clark.cs
+++ Assets/scripts/Balls/Clark.cs
@@ -6,19 +6,38 @@ public class Clark : MonoBehaviour
 {
     [SerializeField]
     private Animator _animator;
+    private Rigidbody _ballRigidBody;
+    private bool _isRunning = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        if (gameObject.layer == 5) return; // ui
         GameManager.Instance.Shooting.AddListener(OnShoot);
+        var ball = GameObject.FindGameObjectWithTag("BALL");
+        _ballRigidBody = ball.GetComponent<Rigidbody>();
     }
 
     private void OnShoot()
     {
+        if (gameObject.layer == 5) return; // ui
+        _isRunning = true;
         _animator.SetBool("isRunning", true);
+        _animator.speed = 3;
+    }
+
+    void Update()
+    {
+        if (!_isRunning) return;
+
+        if (gameObject.layer == 5) return; // ui
+
+        _animator.speed = _ballRigidBody.velocity.magnitude / 40f;
     }
 
     void Destroy()
     {
+        if (gameObject.layer == 5) return; // ui
         GameManager.Instance.Shooting.RemoveListener(OnShoot);
     }
 
